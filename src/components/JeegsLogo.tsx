ff85ac6e120d915f66542c86bb5753865f7936d2
Jhonn <jhonngutierrez.contact@gmail.com>
2024-02-18 13:01:19 -0500

    changes

---
+++ src/components/JeegsLogo.tsx
@@ -0,0 +1,26 @@
+const JeegsLogo = () => {
+  return (
+    <svg
+      height="1em"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17.0017 3.54262C17.0642 3.04219 17.4896 2.66666 17.9939 2.66666H24.8673C25.4688 2.66666 25.9342 3.19384 25.8596 3.79069L23.1047 25.8295C22.8545 27.8312 21.1529 29.3333 19.1356 29.3333H13.7778L17.0017 3.54262Z"
+        stroke="black"
+        stroke-width="2"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7.11117 19.8889L6.52563 24.866C6.24587 27.2439 8.10386 29.3333 10.4982 29.3333H13.7778"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default JeegsLogo;
